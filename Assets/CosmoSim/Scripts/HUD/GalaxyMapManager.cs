2741e0ea88085c0223fe3edd35a8c6015ad813b9
test <test>
2023-08-23 16:11:11 +0200

    grid map

---
+++ Assets/CosmoSim/Scripts/HUD/GalaxyMapManager.cs
@@ -0,0 +1,135 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GalaxyMapManager : MonoBehaviour
+{
+    public enum GalaxyView
+    {
+        TopView,
+        SideView
+    }
+
+    public GameObject galaxyMapObject;
+    public Image galaxyImage;
+
+    public float zoomLevel = 1.0f;
+    public Vector2 panning = Vector2.zero;
+    public bool showNet = true;
+
+    public GalaxyView galaxyView = GalaxyView.TopView;
+    
+    
+    public Text sectorText;
+    public Sprite galaxyTop, galaxySide;
+
+    public LineRendererHUD gridHUD;
+
+    Vector2 touchStart;
+    Rect startRect;
+    float targetZoom;
+    public float zoomOutMin = 1;
+    public float zoomOutMax = 8;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3[] corners = new Vector3[4];
+        galaxyImage.rectTransform.GetWorldCorners(corners);
+        startRect = new Rect(corners[0], corners[2] - corners[0]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3[] corners = new Vector3[4];
+        galaxyImage.rectTransform.GetWorldCorners(corners);
+        Rect newRect = new Rect(corners[0], corners[2] - corners[0]);
+
+        float xPositionDeltaPoint = Input.mousePosition.x - newRect.x;
+        float yPositionDeltaPoint = Input.mousePosition.y - newRect.y;
+
+        //Debug.Log("The x position delta is: " + xPositionDeltaPoint);
+        //Debug.Log("The y position delta is: " + yPositionDeltaPoint);
+        ManagePanAndZoom();
+    }
+
+    private void LateUpdate()
+    {
+        
+    }
+
+    void ManagePanAndZoom()
+    {
+        //Calculates current position
+        Vector3[] corners = new Vector3[4];
+        galaxyImage.rectTransform.GetWorldCorners(corners);
+        Rect newRect = new Rect(corners[0], corners[2] - corners[0]);
+
+        float xPositionDeltaPoint = Input.mousePosition.x - newRect.x;
+        float yPositionDeltaPoint = Input.mousePosition.y - newRect.y;      
+
+        //Calculate desplacement
+        if (Input.GetMouseButtonDown(0))
+        {
+            touchStart.x = xPositionDeltaPoint;
+            touchStart.y = yPositionDeltaPoint;
+        }        
+        else if (Input.GetMouseButton(0))
+        {
+            Vector3 direction = touchStart - new Vector2(xPositionDeltaPoint, yPositionDeltaPoint);
+            galaxyImage.rectTransform.position -= direction;
+        }
+
+        //Read mouse wheel and apply zoom
+        float increment = -Input.GetAxis("Mouse ScrollWheel");
+
+        float zoomFactor = Mathf.Clamp(galaxyImage.rectTransform.localScale.x + increment, zoomOutMin, zoomOutMax);
+
+        //calculates displacement when zoom
+        Vector2 correction = new Vector2(xPositionDeltaPoint, yPositionDeltaPoint);
+        Vector2 newPos = correction * (zoomFactor / galaxyImage.rectTransform.localScale.x);
+        correction = newPos - correction;
+
+        
+        gridHUD.SetNewGridFactor(((galaxyImage.rectTransform.localScale.x-1) % 2) + 1);
+
+        //Apply zoom
+        zoom(zoomFactor);
+
+        //Apply translation and correction to zoom to mouse position        
+        Debug.Log( increment);
+
+
+        float factor = Mathf.Clamp(galaxyImage.rectTransform.localScale.x, zoomOutMin, zoomOutMax);
+        Vector2 limits = startRect.size * (factor -1f);
+        
+        Vector2 correctedPosition = galaxyImage.rectTransform.position;
+
+        correctedPosition -= startRect.position;
+        //Debug.Log(factor + " - " + limits + " - " + galaxyImage.rectTransform.position + " - " + correctedPosition);
+
+        
+        if (correctedPosition.x < -limits.x)
+            correctedPosition.x = -limits.x;
+
+        if (correctedPosition.x > 0)
+            correctedPosition.x = 0;
+
+        if (correctedPosition.y < -limits.y)
+            correctedPosition.y = -limits.y;
+
+        if (correctedPosition.y > 0)
+            correctedPosition.y = 0;
+        
+
+        galaxyImage.rectTransform.position = correctedPosition +  startRect.position - correction;
+    }
+
+    void zoom(float factor)
+    {     
+        galaxyImage.rectTransform.localScale = new Vector3(factor, factor, 0);
+    }
+}
---
+++ Assets/CosmoSim/Scripts/HUD/LineRendererHUD.cs
@@ -0,0 +1,210 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LineRendererHUD : Graphic
+{
+
+    public Vector2 gridSize;
+    public Vector2 deltaOrigin;
+    public float thickness=10f;
+    public float gridFactor = 1f;
+
+    float width;
+    float height;
+    float celltWidth;
+    float cellHeight;
+
+    public void SetNewGridFactor(float factor)
+    {
+        if (gridFactor == factor)
+            return;
+        gridFactor=factor;
+        SetAllDirty();
+    }
+
+    protected override void OnPopulateMesh(VertexHelper vh)
+    {
+
+        vh.Clear();
+
+        Vector2 finalGridSize =gridSize * gridFactor;
+
+        width = rectTransform.rect.width;
+        height = rectTransform.rect.height;
+
+        celltWidth = width / finalGridSize.x;
+        cellHeight = height / finalGridSize.y;
+
+        int count = 0;
+
+        /* old method
+        for(int y=0; y<gridSize.y; y++)
+        {
+            for(int x=0; x<gridSize.x; x++)
+            {
+                DrawCell(x, y, count, vh);
+                count++;
+            }
+        }
+        */
+
+        float xOrigin = deltaOrigin.x % finalGridSize.x;
+        if (xOrigin < 0)
+            xOrigin += finalGridSize.x;
+        for (float x = xOrigin; x < width; x+= finalGridSize.x)
+        {
+            DrawYAxis((float)x, count, vh);
+            count++;
+        }
+
+        float yOrigin = deltaOrigin.y % finalGridSize.y;
+        if (yOrigin < 0)
+            yOrigin += finalGridSize.y;
+
+        for (float y = yOrigin; y < height; y += finalGridSize.y)
+        {
+            DrawXAxis((float)y, count, vh);
+            count++;
+        }
+
+        /*
+        for (int x = 0; x <= gridSize.x; x++)
+        {
+            DrawYAxis((float)x * celltWidth, count, vh);
+            count++;
+        }
+
+        for (int y = 0; y <= gridSize.y; y++)
+        {
+            DrawXAxis((float)y * cellHeight, count, vh);
+            count++;
+        }
+        */
+    }
+
+
+    private void DrawXAxis(float origin, int index, VertexHelper vh)
+    {
+
+        float xPos = 0;
+        float yPos = origin;
+
+        UIVertex vertex = UIVertex.simpleVert;
+        vertex.color = color;
+
+        vertex.position = new Vector3(xPos, yPos);
+        vh.AddVert(vertex);
+
+        vertex.position = new Vector3(xPos + width, yPos );
+        vh.AddVert(vertex);
+
+        float widthSqr = thickness * thickness;
+        float distanceSqr = widthSqr / 2f;
+
+        float distance = Mathf.Sqrt(distanceSqr);
+
+        vertex.position = new Vector3(xPos, yPos + distance);
+        vh.AddVert(vertex);
+
+        vertex.position = new Vector3(xPos + width, yPos + distance);
+        vh.AddVert(vertex);
+
+        int offset = index * 4;
+
+        vh.AddTriangle(offset + 0, offset + 1, offset + 2);
+        vh.AddTriangle(offset + 1, offset + 3, offset + 2);
+
+    }
+
+    private void DrawYAxis(float origin, int index, VertexHelper vh)
+    {
+
+        float xPos = origin;
+        float yPos = 0;
+
+        UIVertex vertex = UIVertex.simpleVert;
+        vertex.color = color;
+
+        vertex.position = new Vector3(xPos, yPos);
+        vh.AddVert(vertex);
+
+        vertex.position = new Vector3(xPos, yPos + height);
+        vh.AddVert(vertex);
+
+        float widthSqr = thickness * thickness;
+        float distanceSqr = widthSqr / 2f;
+
+        float distance = Mathf.Sqrt(distanceSqr);
+
+        vertex.position = new Vector3(xPos + distance, yPos);
+        vh.AddVert(vertex);
+
+        vertex.position = new Vector3(xPos + distance, yPos + height);
+        vh.AddVert(vertex);
+
+        int offset = index * 4;
+
+        vh.AddTriangle(offset + 0, offset + 1, offset + 2);
+        vh.AddTriangle(offset + 1, offset + 3, offset + 2);
+
+    }
+
+    private void DrawCell(int x, int y, int index, VertexHelper vh)
+    {
+
+        float xPos = celltWidth * x;
+        float yPos = cellHeight * y;
+
+        UIVertex vertex = UIVertex.simpleVert;
+        vertex.color = color;
+
+        vertex.position = new Vector3(xPos, yPos);
+        vh.AddVert(vertex);
+
+        vertex.position = new Vector3(xPos, yPos + cellHeight);
+        vh.AddVert(vertex);
+
+        vertex.position = new Vector3(xPos + celltWidth, yPos + cellHeight);
+        vh.AddVert(vertex);
+
+        vertex.position = new Vector3(xPos + celltWidth, yPos);
+        vh.AddVert(vertex);
+
+        //vh.AddTriangle(0, 1, 2);
+        //vh.AddTriangle(2, 3, 0);
+
+        float widthSqr = thickness * thickness;
+        float distanceSqr = widthSqr / 2f;
+
+        float distance = Mathf.Sqrt(distanceSqr);
+
+        vertex.position = new Vector3(xPos + distance, yPos + distance);
+        vh.AddVert(vertex);
+
+        vertex.position = new Vector3(xPos + distance, yPos +(cellHeight - distance));
+        vh.AddVert(vertex);
+
+        vertex.position = new Vector3(xPos + (celltWidth - distance), yPos + (cellHeight - distance));
+        vh.AddVert(vertex);
+
+        vertex.position = new Vector3(xPos + (celltWidth - distance), yPos + distance);
+        vh.AddVert(vertex);
+
+        int offset = index * 8;
+
+        vh.AddTriangle(offset + 0, offset + 1, offset + 5);
+        vh.AddTriangle(offset + 5, offset + 4, offset + 0);
+
+        vh.AddTriangle(offset + 1, offset + 2, offset + 6);
+        vh.AddTriangle(offset + 6, offset + 5, offset + 1);
+
+        vh.AddTriangle(offset + 2, offset + 3, offset + 7);
+        vh.AddTriangle(offset + 7, offset + 6, offset + 2);
+
+        vh.AddTriangle(offset + 3, offset + 0, offset + 4);
+        vh.AddTriangle(offset + 4, offset + 7, offset + 3);
+
+    }
+}
